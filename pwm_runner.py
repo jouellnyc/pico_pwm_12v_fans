ae2509fec97ef1ec360de745a8aefdad8792beb6
JohnO <jouellnyc@gmail.com>
2023-10-01 19:10:35 -0400

    More Tech Specs

--- pwm_runner.py
+++ pwm_runner.py
@@ -18,13 +18,17 @@ Other logical reference https://www.bananarobotics.com/shop/How-to-use-the-HG788
 
 """
 
-from machine import Pin
+
+from machine import Pin, PWM
 import time
 
-""" The normal full power operation of the DC hobby fan is clockwise """ 
-""" when holding the fan with the black wire on the left             """
+B1A = Pin(0, Pin.OUT)
+B1B = Pin(1, Pin.OUT)
 
-#3-5V DC Hobby Fan
+""" 3-5V DC Hobby Fan 
+The normal full power operation of the DC hobby fan is clockwise  
+when holding the fan with the black wire on the left
+
 #Forward
 #>>> B1A.value(0); B1B.value(1) 
 #Stop
@@ -33,25 +37,17 @@ import time
 #>>> B1A.value(1); B1B.value(0)
 #Stop
 #>>> B1A.value(0); B1B.value(0)  
-
-#12V DC Fan
-# https://www.aliexpress.us/item/3256802262312152.html
-#Forward
-#>>> B1A.value(0); B1B.value(1) 
-#All other combos do not make any difference as it appears to only
-#Move in one direction
-
-
-#Pysical Labels
-B1A = Pin(0, Pin.OUT)
-B1B = Pin(1, Pin.OUT)
-
-"""  NO PWM UP TO  THIS POINT !!! """
-#Logical Labels
-#direction = B1A
-#speed     = B1B
-
 """
+
+""" 12V DC Fan 
+#Forward
+>>> B1A.value(0); B1B.value(1) 
+All other combos do not make any difference as it appears to only move in one direction
+However, I can put full 12V DC on the L9110 and the pi/pico/everything is stable.
+"""
+
+"""  NO PWM UP TO  THIS POINT !!! 
+
 Software: I use 1 pin as direction pin and the other pin as speed pin.
 If the direction is low then the motor will get power when ever the speed pin is high
 so the speed the motor will spin at will be the duty with of the speed pin i.e PWM_duty = desired_speed.
@@ -60,31 +56,28 @@ If the direction is high then motor will spin the other way and the motor will g
 low so you have to invert the PWM duty i.e PWM_duty = 100 - desired_speed
 """
 
-#Set direction to low
-#direction.value(0)
-#Left most connection
-B1A.value(1)
+#Left most connection on the L9110
+B1A.value(0)
 #Immediate to the right of B1A
-B1B.value(0); 
-pwm_speed = machine.PWM(B1B, freq=1000, duty_u16=0)
+B1B.value(1); 
+pwm_speed = PWM(B1B, freq=1000, duty_u16=0)
 
 """
 #Stop 
 pwm_speed.duty_u16(0)
+pwm.deinit()
 #Full on 
 pwm_speed.duty_u16(65535)
-
-pwm.deinit()
 """
 
 while True:
     
-    for x in range(25000,65535,500):
+    for x in range(25000,65535,1000):
         print(f"== {x} ==")
         pwm_speed.duty_u16(x)
         time.sleep(1)
         
-    for x in range(65535,25000,-2000):
+    for x in range(65535,25000,-1000):
         print(f"== {x} ==")
         pwm_speed.duty_u16(x)
         time.sleep(1)
